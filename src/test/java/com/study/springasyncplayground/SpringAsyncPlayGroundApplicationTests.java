b2118985aa7789ca24c5503c1e28d908ebae8c75
jonghyunhub <chuwhdgus@gmail.com>
2024-08-09 19:34:02 +0900

    Initial commit

---
+++ src/test/java/com/study/springasyncplayground/SpringAsyncPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springasyncplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAsyncPlayGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
